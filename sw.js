5ff759dde42e53dec895c199ceaf7dafed97ba3c
skeelzer <lucas.maxime.m.jacquot@gmail.com>
2026-05-27 14:11:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── SERVICE WORKER — Cache-first strategy for full offline support ─────────────
-const CACHE = 'faluche-v6';
+const CACHE = 'faluche-v7';
 const FILES = [
   './',
   './index.html',
